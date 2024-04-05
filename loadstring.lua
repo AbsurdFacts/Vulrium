becb8aa583ca2f24797cb58946b9e984e1122ceb
AbsurdFacts <163673134+AbsurdFacts@users.noreply.github.com>
2024-04-05 15:03:18 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Meta91YT/Vulrium/main/MainScript.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AbsurdFacts/Vulrium/main/MainScript.lua", true))()
